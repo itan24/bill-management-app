91ae85de7014926bc8bd56701b7f11f32752adbf
Muhammad Tanveer <tanveer.tan9091@gmail.com>
2025-05-04 14:49:41 +0500

    Force cache clear

--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,25 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+// middleware.ts
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in",
+  "/sign-up",
+  "/sign-out",
+  "/get-token",
+  "/test-api",
+  "/api/(.*)", // Allow backend API calls
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    const session = await auth();
+    if (!session.userId) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+  }
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
-
-
-// // middleware.ts
-// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
-// const isPublicRoute = createRouteMatcher([
-//   "/",
-//   "/sign-in",
-//   "/sign-up",
-//   "/sign-out",
-//   "/get-token",
-//   "/test-api",
-//   "/api/(.*)", // Allow backend API calls
-// ]);
-
-// export default clerkMiddleware(async (auth, req) => {
-//   if (!isPublicRoute(req)) {
-//     const session = await auth();
-//     if (!session.userId) {
-//       return new Response("Unauthorized", { status: 401 });
-//     }
-//   }
-// });
-
-// export const config = {
-//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
